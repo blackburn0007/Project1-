892bd6e7aa2f3805b68a604d51031904aaf88f88
Siyavush <snajmudinov@gmail.com>
2021-03-25 17:10:28 +0500

    automated questions using for loop

--- script.js
+++ script.js
@@ -15,12 +15,11 @@ const personalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i < 2 ; i++) {
+    const lastSeen = prompt("Какой фильм вы посмотрели в последний раз?",""),
+    lastSeenRank = +prompt("На сколько оцените его?","");
+    
+    personalMovieDB.movies[lastSeen] = lastSeenRank;
+}
+
 console.log(personalMovieDB);
-
-const lastSeen = prompt("Какой фильм вы посмотрели в последний раз?",""),
-      lastSeenRank = prompt("На сколько оцените его?",""),
-      lastSeen2 = prompt("Какой фильм вы посмотрели в последний раз?",""),
-      lastSeenRank2 = prompt("На сколько оцените его?","");
-
-personalMovieDB.movies[lastSeen] = lastSeenRank;
-personalMovieDB.movies[lastSeen2] = lastSeenRank2;
