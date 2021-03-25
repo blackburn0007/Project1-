1b72889ac128936bbef3657b8f9ebe5f58ffc322
Siyavush <snajmudinov@gmail.com>
2021-03-25 22:53:39 +0500

    added conditions for prompt

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 "Use strict";
 
-
-const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели7","");
+const numberOfFilms = prompt ("How many movies have you seen?","");
 console.log(numberOfFilms);
-
-
-
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    private: false
 };
 
 for (let i = 0; i < 2 ; i++) {
-    const lastSeen = prompt("Какой фильм вы посмотрели в последний раз?",""),
-    lastSeenRank = +prompt("На сколько оцените его?","");
+    const lastSeenMovie = prompt("What movie have you seen recently?",""),
+          lastSeenMovieRank = +prompt("How would you rate the movie on a scale of 1 to 10?","");
     
-    personalMovieDB.movies[lastSeen] = lastSeenRank;
+    if (lastSeenMovie != null && lastSeenMovieRank != null && lastSeenMovie != "" && lastSeenMovieRank != "" && lastSeenMovie.length < 50) {
+        personalMovieDB.movies[lastSeenMovie] = lastSeenMovieRank;
+        console.log('success');
+    } else {
+        i--;
+        console.log("error");
+    }
 }
-
 console.log(personalMovieDB);
+
