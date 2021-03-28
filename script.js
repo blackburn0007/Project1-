9d990004b4c592a9ca9ea4ac604e11217227cc6e
Siyavush <snajmudinov@gmail.com>
2021-03-28 12:07:49 +0500

    Used functions for all blocks of code, created a function for checking private database and created function with genre prompts

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 "Use strict";
 
-const numberOfFilms = +prompt ("How many movies have you seen?","");
-console.log(numberOfFilms);
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt ("How many movies have you seen?","");
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN (numberOfFilms)) {
+        numberOfFilms = +prompt ("How many movies have you seen?","");
+    }
+}
+
+// start();
 
 
 const personalMovieDB = {
@@ -9,50 +18,57 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: true
 };
 
-if (personalMovieDB.count < 10 ) {
-    console.log("You have seen few movies");
-}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-    console.log(" You are a classic viewer");
-}else if (personalMovieDB.count >= 30) {
-    console.log("You are a moviegoer");
-}else {
-    console.log("An error happened");
+
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10 ) {
+        console.log("You have seen few movies");
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+        console.log(" You are a classic viewer");
+    }else if (personalMovieDB.count >= 30) {
+        console.log("You are a moviegoer");
+    }else {
+        console.log("An error happened");
+    }
 }
 
+// detectPersonalLevel();
 
 
+function rememberMyMovies () {
+    for (let i = 0; i < 2 ; i++) {
+        const lastSeenMovie = prompt("What movie have you seen recently?",""),
+              lastSeenMovieRank = +prompt("How would you rate the movie on a scale of 1 to 10?","");
+        
+        if (lastSeenMovie != null && lastSeenMovieRank != null && lastSeenMovie != "" && lastSeenMovieRank != "" && lastSeenMovie.length < 50) {
+            personalMovieDB.movies[lastSeenMovie] = lastSeenMovieRank;
+            console.log('success');
+        } else {
+            i--;
+            console.log("error");
+        }
+    }
+}
 
-// for (let i = 0; i < 2 ; i++) {
+// rememberMyMovies();
+
+// let i = 0;
+// do {
 //     const lastSeenMovie = prompt("What movie have you seen recently?",""),
-//           lastSeenMovieRank = +prompt("How would you rate the movie on a scale of 1 to 10?","");
-    
-//     if (lastSeenMovie != null && lastSeenMovieRank != null && lastSeenMovie != "" && lastSeenMovieRank != "" && lastSeenMovie.length < 50) {
-//         personalMovieDB.movies[lastSeenMovie] = lastSeenMovieRank;
-//         console.log('success');
-//     } else {
-//         i--;
-//         console.log("error");
-//     }
+//       lastSeenMovieRank = +prompt("How would you rate the movie on a scale of 1 to 10?","");
+//   i++;
+//   if (lastSeenMovie != null && lastSeenMovieRank != null && lastSeenMovie != "" && lastSeenMovieRank != "" && lastSeenMovie.length < 50) {
+//                     personalMovieDB.movies[lastSeenMovie] = lastSeenMovieRank;
+//                     console.log('success');
+//                 } else {
+//                     i--;
+//                     console.log("error");
+//                 }
 // }
-
-
-let i = 0;
-do {
-    const lastSeenMovie = prompt("What movie have you seen recently?",""),
-      lastSeenMovieRank = +prompt("How would you rate the movie on a scale of 1 to 10?","");
-  i++;
-  if (lastSeenMovie != null && lastSeenMovieRank != null && lastSeenMovie != "" && lastSeenMovieRank != "" && lastSeenMovie.length < 50) {
-                    personalMovieDB.movies[lastSeenMovie] = lastSeenMovieRank;
-                    console.log('success');
-                } else {
-                    i--;
-                    console.log("error");
-                }
-}
-while (i < 2);
+// while (i < 2);
 
 // Using while loop to do the same thing
 // let i = 0;
@@ -70,6 +86,22 @@ while (i < 2);
 // }
 
 
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB (personalMovieDB.private);
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        let favoriteGenre = [];
+         favoriteGenre = prompt(`Your favorite movie genre under number ${i}`);
+         personalMovieDB.genres[i -1] = favoriteGenre;
+    }
+}
+
+writeYourGenres();
 
 console.log(personalMovieDB);
-
